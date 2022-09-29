64d75fc704d3285aea7a8899523b4ed06305d2cd
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-09-29 10:54:44 -0400

    added isDivisibleBy

--- funcs.cpp
+++ funcs.cpp
@@ -2,4 +2,13 @@
 #include "funcs.h"
 
 // add functions here
-
+bool isDivisibleBy(int n, int d){
+	if(n % d == 0) {
+		cout << "Yes" << endl;
+		return true;
+	}
+	else {
+		cout << "Yes" << endl;
+		return false;
+	}
+}
