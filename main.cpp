1741a106f0509e288989f16ead9865ca2f2f9db5
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-09-29 13:13:09 -0400

    added some functions

--- main.cpp
+++ main.cpp
@@ -5,5 +5,17 @@
 
 int main()
 {
-  return 0;
+	isDivisibleBy(100, 25);
+	isDivisibleBy(35, 17);	
+
+	isPrime(4);
+	isPrime(5);
+	
+	std::cout << nextPrime(3) << std::endl;
+	std::cout << nextPrime(14) << std::endl;
+	std::cout << nextPrime(17) << std::endl;
+
+	std::cout << countPrimes(5, 25) << std::endl;
+
+	return 0;
 }
