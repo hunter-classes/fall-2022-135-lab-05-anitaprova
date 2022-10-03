5a2a2eb6da467c08732388dbe231f4424241aabf
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-10-03 11:17:56 -0400

    fixed the output

--- main.cpp
+++ main.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
-
-
 #include "funcs.h"
 
 int main()
 {
-	isDivisibleBy(100, 25);
-	isDivisibleBy(35, 17);	
+	std::cout << isDivisibleBy(100, 25) << std::endl;
+	std::cout << isDivisibleBy(35, 17) << std::endl;	
 
-	isPrime(4);
-	isPrime(5);
+	std::cout << isPrime(4) << std::endl;
+	std::cout << isPrime(5) << std::endl;
 	
 	std::cout << nextPrime(3) << std::endl;
 	std::cout << nextPrime(14) << std::endl;
 	std::cout << nextPrime(17) << std::endl;
-
+	
 	std::cout << countPrimes(5, 25) << std::endl;
 		
 	std::cout << isTwinPrime(15) << std::endl;
@@ -23,7 +21,7 @@ int main()
 	std::cout << nextTwinPrime(17) << std::endl;
 
 	std::cout << largestTwinPrime(5, 18) << std::endl;
-	std::cout << largestTwinPrime(14, 16) << std::endl;	
+	std::cout << largestTwinPrime(14, 16) << std::endl;
 
 	return 0;
 }
